c5b3a6ffb469ccb7372afea94da275250c1e6f55
Adriana <adrianam754@gmail.com>
2022-07-20 21:41:02 -0300

    refactor: config cors and routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
+
+import { routerCompany } from './routes/company'
+import { routerProduct } from './routes/product'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
+
+app.use(routerCompany)
+app.use(routerProduct)
 
 export default app
