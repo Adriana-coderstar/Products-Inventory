00ec01da907b18962f1794398b46b7bc69ec448b
Adriana <adrianam754@gmail.com>
2022-07-20 21:42:13 -0300

    feat: create routes from company table

---
+++ src/routes/company.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import CompanyController from '../controller/CompanyController'
+
+export const routerCompany = Router()
+
+routerCompany.post('./company', CompanyController.create)
+routerCompany.get('./company', CompanyController.findAll)
+routerCompany.get('./company', CompanyController.findOne)
+routerCompany.put('./company', CompanyController.update)
+routerCompany.delete('./company', CompanyController.delete)
