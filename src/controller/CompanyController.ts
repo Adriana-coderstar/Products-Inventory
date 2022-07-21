09ce6c0507ba2a13a2afaae63e8d98b002e58e23
Adriana <adrianam754@gmail.com>
2022-07-21 00:03:35 -0300

    feat: add create company in controller

--- src/controller/CompanyController.ts
+++ src/controller/CompanyController.ts
@@ -2,7 +2,22 @@ import { Request, Response, NextFunction } from 'express'
 import CompanyService from '../service/CompanyService'
 
 class Company {
-  async create (req: Request, resp: Response, next: NextFunction) {}
+  async create (req: Request, resp: Response, next: NextFunction):
+  Promise<void> {
+    try {
+      const { company } = req.body
+
+      const newCompany = await CompanyService.create(company)
+
+      newCompany instanceof Error
+        ? resp.status(400).json({ error: newCompany.message })
+        : resp.status(201).json(newCompany)
+    } catch (error) {
+      console.log(error)
+      return next(error)
+    }
+  }
+
   async findAll (req: Request, resp: Response, next: NextFunction) {}
   async findOne (req: Request, resp: Response, next: NextFunction) {}
   async update (req: Request, resp: Response, next: NextFunction) {}
