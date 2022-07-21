f91732a5a726089cb0164d1aca623f0e1d67cabd
Adriana <adrianam754@gmail.com>
2022-07-21 00:06:18 -0300

    feat: add the route create companies

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -3,8 +3,9 @@ import CompanyController from '../controller/CompanyController'
 
 export const routerCompany = Router()
 
-routerCompany.post('./company', CompanyController.create)
-routerCompany.get('./company', CompanyController.findAll)
-routerCompany.get('./company', CompanyController.findOne)
-routerCompany.put('./company', CompanyController.update)
-routerCompany.delete('./company', CompanyController.delete)
+routerCompany.post('/company', CompanyController.create)
+
+// routerCompany.get('./company', CompanyController.findAll)
+// routerCompany.get('./company', CompanyController.findOne)
+// routerCompany.put('./company', CompanyController.update)
+// routerCompany.delete('./company', CompanyController.delete)
