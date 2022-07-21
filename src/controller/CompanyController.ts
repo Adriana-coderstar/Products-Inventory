b76949d60d7cc7ea2569a86102b465b53bf61c6f
Adriana <adrianam754@gmail.com>
2022-07-20 23:54:41 -0300

    feat: import CompanyService

--- src/controller/CompanyController.ts
+++ src/controller/CompanyController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
+import CompanyService from '../service/CompanyService'
 
 class Company {
   async create (req: Request, resp: Response, next: NextFunction) {}
