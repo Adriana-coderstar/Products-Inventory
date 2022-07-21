e43ea84cd32c41da53c1826e3b39e07452d5e612
Adriana <adrianam754@gmail.com>
2022-07-20 21:42:57 -0300

    feat: create routes from product table

---
+++ src/routes/product.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { ProductController } from '../controller/ProductController'
+
+export const routerProduct = Router()
+
+routerProduct.post('./product', ProductController.create)
+routerProduct.get('./product', ProductController.findAll)
+routerProduct.get('./product', ProductController.findOne)
+routerProduct.put('./product/:id', ProductController.update)
+routerProduct.delete('./product/:id', ProductController.delete)
