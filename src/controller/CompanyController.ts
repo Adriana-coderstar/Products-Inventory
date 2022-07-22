8b26571f2f5a3ae318af1e3815950e6df23f9fed
Adriana <adrianam754@gmail.com>
2022-07-22 09:43:57 -0300

    feat: add company request in controller

--- src/controller/CompanyController.ts
+++ src/controller/CompanyController.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
 import CompanyService from '../service/CompanyService'
+import { Providers } from '@prisma/client'
 
 class Company {
   async create (req: Request, resp: Response, next: NextFunction):
-  Promise<void> {
+  Promise<Providers | any> {
     try {
       const { company } = req.body
 
@@ -12,16 +13,67 @@ class Company {
       newCompany instanceof Error
         ? resp.status(400).json({ error: newCompany.message })
         : resp.status(201).json(newCompany)
-    } catch (error) {
-      console.log(error)
-      return next(error)
+    } catch (error: any) {
+      resp.status(500).json({ message: error.message })
     }
   }
 
-  async findAll (req: Request, resp: Response, next: NextFunction) {}
-  async findOne (req: Request, resp: Response, next: NextFunction) {}
-  async update (req: Request, resp: Response, next: NextFunction) {}
-  async delete (req: Request, resp: Response, next: NextFunction) {}
+  async findAll (req: Request, resp: Response, _next: NextFunction):
+  Promise<Providers[] | any> {
+    try {
+      const allCompanies = await CompanyService.findAll()
+
+      return resp.status(200).json(allCompanies)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async findOne (req: Request, resp: Response, _next: NextFunction):
+  Promise<Providers | any> {
+    try {
+      const { company } = req.body
+
+      const findCompany = await CompanyService.findOne(company)
+
+      findCompany instanceof Error
+        ? resp.status(400).json({ error: company.message })
+        : resp.status(200).json(findCompany)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async update (req: Request, resp: Response, _next: NextFunction):
+  Promise<Providers | any> {
+    try {
+      const { id } = req.params
+      const { company } = req.body
+
+      const updateCompany = await CompanyService.update(Number(id),company)
+
+      updateCompany instanceof Error
+        ? resp.status(400).json({ message: updateCompany.message })
+        : resp.status(200).json(updateCompany)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async delete (req: Request, resp: Response, _next: NextFunction):
+  Promise<string | any> {
+    try {
+      const { company } = req.body
+
+      const deleteCompany = await CompanyService.delete(company)
+
+      deleteCompany instanceof Error
+        ? resp.status(400).json({ message: deleteCompany.message })
+        : resp.status(200).send({ message: 'Successfully deleted' })
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
 
 export default new Company()
