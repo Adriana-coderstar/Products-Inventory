c7f7631a772d8a989ae5c645827d5c5d0d91450f
Adriana <adrianam754@gmail.com>
2022-07-22 01:29:39 -0300

    chore: install, config jest and supertest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable @typescript-eslint/semi */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  detectOpenHandles: true,
+}
