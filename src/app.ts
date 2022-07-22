d4568b04c60ebe4053648e79a7ec8aac31eff024
Adriana <adrianam754@gmail.com>
2022-07-22 09:54:23 -0300

    feat: add a request to test endpoint error

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 
-import { routerCompany } from './routes/company'
-import { routerProduct } from './routes/product'
+import { routerCompany } from './routes/company.routes'
+import { routerProduct } from './routes/product.routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 
 app.use(routerCompany)
 app.use(routerProduct)
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: 'Endpoint not found' })
+})
 
 export default app
