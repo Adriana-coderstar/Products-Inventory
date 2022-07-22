d22be9b117acb8e2db9c04ba23df8e883b124ccf
Adriana <adrianam754@gmail.com>
2022-07-22 09:59:27 -0300

    feat: add product route

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express'
 import ProductController from '../controller/ProductController'
+import ProductValidate from '../middleware/ValidateProduct'
 
 export const routerProduct = Router()
 
-routerProduct.post('./product', ProductController.create)
-routerProduct.get('./product', ProductController.findAll)
-routerProduct.get('./product', ProductController.findOne)
-routerProduct.put('./product/:id', ProductController.update)
-routerProduct.delete('./product/:id', ProductController.delete)
+routerProduct.post('/product',ProductValidate.validateError,
+  ProductController.create)
+
+routerProduct.get('/allProduct',ProductValidate.validateFindAll,
+  ProductController.findAll)
+
+routerProduct.get('/product',ProductValidate.validateFindOne,
+  ProductController.findOne)
+
+routerProduct.put('/product/:id',ProductValidate.validateError,
+  ProductController.update)
+
+routerProduct.delete('/product',ProductValidate.validateFindOne,
+  ProductController.delete)
